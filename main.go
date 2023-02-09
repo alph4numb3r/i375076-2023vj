4fa5ed285d7bb2065f8b0739f98177594fe4bae3
HeadsetRuler <alph4numb3r@gmail.com>
2023-02-09 12:39:12 +0100

    initial v2

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// time constants
+const (
+        MorningThreshold = 7
+        AfternoonThreshold = 12
+        EveningThreshold = 18
+        NightThreshold = 23
+)
+
+// message constants
+const (
+        DefaultMessage = "! Welkom bij Fonteyn Vakantieparken"
+        MorningPrefix = "Goedemorgen"
+        AfternoonPrefix = "Goedemiddag"
+        EveningPrefix = "Goedenavond"
+        MorningMessage = MorningPrefix + DefaultMessage
+        AfternoonMessage = AfternoonPrefix + DefaultMessage
+        EveningMessage = EveningPrefix + DefaultMessage
+        NightMessage = "Sorry, de parkeerplaats is ’s nachts gesloten"
+)
 
 func main() {
-    fmt.Println("Welkom in mijn persoonlijke hel!")
+    time := time.Now()
+
+    switch {
+    case time.Hour() < MorningThreshold:
+        fmt.Println(NightMessage)
+    case time.Hour() < AfternoonThreshold:
+        fmt.Println(MorningMessage)
+    case time.Hour() < EveningThreshold:
+        fmt.Println(AfternoonMessage)
+    case time.Hour() < NightThreshold:
+        fmt.Println(EveningMessage)
+    default:
+        fmt.Println(NightMessage)
+    }
 }
