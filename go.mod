b390e1021bf9e79d6c1350c7f256bd7789fd2940
HeadsetRuler <alph4numb3r@gmail.com>
2023-02-10 11:41:41 +0100

    added license plate reader functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alph4numb3r/i375076-2023vj
 
 go 1.20
+
+require github.com/deckarep/golang-set/v2 v2.1.0
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"time"
+
+	Set "github.com/deckarep/golang-set/v2"
 )
 
 // time constants
@@ -25,35 +29,73 @@ const (
         NightMessage = "Sorry, de parkeerplaats is ’s nachts gesloten"
 )
 
+//license plates
+type TLicensePlates Set.Set[string]
+
 // debug constants
-/*
 const (
-        debugOffset = 6
+        //debugOffset = 6
 )
-*/
+func debugLicensePlates() []string {
+    return []string{
+        "1-ABC-123",
+        "2-DEF-456",
+        "3-GHI-789",
+        "4-JKL-012",
+        "5-MNO-345",
+        "6-PQR-678",
+        "7-STU-901",
+        "8-VWX-234",
+    }
+}
+
+
+
+
+
 
 func main() {
-    // debug
-    /* 
-    now := time.Now().Add(time.Hour * debugOffset)
-    fmt.Println(now.Clock())
-    */
+    var (
+    // TEST: set time
+    //now = time.Now().Add(time.Hour * debugOffset)
+    // PROD: get time
+    now = time.Now()
 
-    // production
-    ///*
-    now := time.Now()
-    //*/
+    reader = bufio.NewReader(os.Stdin)
+    licensePlates TLicensePlates
+    )
 
-    switch {
-    case now.Hour() < MorningThreshold:
-        fmt.Println(NightMessage)
-    case now.Hour() < AfternoonThreshold:
-        fmt.Println(MorningMessage)
-    case now.Hour() < EveningThreshold:
-        fmt.Println(AfternoonMessage)
-    case now.Hour() < NightThreshold:
-        fmt.Println(EveningMessage)
-    default:
-        fmt.Println(NightMessage)
+
+    // PROD: get license plates from database
+    //licensePlates = 
+    
+    // TEST: hard-coded license plates
+    licensePlates = Set.NewSet(debugLicensePlates()...)
+
+    var licensePlate string
+    // PROD: get license plate from camera
+
+    // TEST: input license plate from console
+    fmt.Print("License plate: ")
+    for err := *new(error); err != nil; licensePlate, err = reader.ReadString('\n') {
+        fmt.Print("License plate: ")
+    }
+
+    // check if license plate is in set
+    if licensePlates.Contains(licensePlate) {
+        switch {
+        case now.Hour() < MorningThreshold:
+            fmt.Println(NightMessage)
+        case now.Hour() < AfternoonThreshold:
+            fmt.Println(MorningMessage)
+        case now.Hour() < EveningThreshold:
+            fmt.Println(AfternoonMessage)
+        case now.Hour() < NightThreshold:
+            fmt.Println(EveningMessage)
+        default:
+            fmt.Println(NightMessage)
+        }
+    } else {
+        fmt.Println("U heeft helaas geen toegang tot het parkeerterrein")
     }
 }
